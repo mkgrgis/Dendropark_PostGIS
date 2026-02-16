0e3e6f2b8e1fe2ba65e5da3b41f6ccdd1af988e6
Михаил <41448637+mkgrgis@users.noreply.github.com>
2026-02-16 14:25:57 +0300

    Update db_init.sh - дополнения

--- test/db_init.sh
+++ test/db_init.sh
@@ -1,2 +1,10 @@
 echo "create user \"$2\" password '$3';" | sudo -u postgres psql;
 echo "create database \"$1\" owner \"$2\";" | sudo -u postgres psql;
+
+echo "CREATE EXTENSION postgis;
+CREATE EXTENSION http;
+CREATE EXTENSION file_fdw;
+
+CREATE SERVER \"Wiki дендропарк\" FOREIGN DATA WRAPPER file_fdw;
+grant usage on foreign server \"Wiki дендропарк\" to \"$2\";
+grant pg_execute_server_program to \"$2\"; " | sudo -u postgres psql -d "$1";
