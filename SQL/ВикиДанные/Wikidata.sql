ca028642ddbbccaf1102518d256f91f23b4f232f
Михаил <41448637+mkgrgis@users.noreply.github.com>
2026-03-17 11:11:07 +0300

    Добавление номера

--- SQL/ВикиДанные/Wikidata.sql
+++ SQL/ВикиДанные/Wikidata.sql
@@ -6,11 +6,13 @@ select
   (http_get(
     'https://query.wikidata.org/sparql?query=' || 
     urlencode(
-      'SELECT ?элемент ?элементLabel ?типДороги ?типДорогиLabel
+      'SELECT ?элемент ?элементLabel ?типДорогиLabel ?№
 WHERE {
-  ?элемент wdt:P361 wd:Q4087179 .         # Бирюлёвский дендропарк
-  ?элемент wdt:P31 ?типДороги .            # тип объекта
-  ?типДороги wdt:P279* wd:Q34442 .          # подкласс дороги (road)
+  ?элемент wdt:P361 wd:Q4087179 ; p:P361 ?stn. # Бирюлёвский дендропарк     
+  OPTIONAL { ?stn pq:P1545 ?№.}                # порядковый номер его части (из паспорта ОКН)
+  ?элемент wdt:P31 ?типДороги .                # тип объекта
+  ?типДороги wdt:P279* ?родитель.
+  FILTER (?родитель IN (wd:Q34442, wd:Q5004679)) . # тип объекта восходит к дорогам или тропам
   SERVICE wikibase:label { bd:serviceParam wikibase:language "ru" . }
 }'
     ) ||
@@ -24,7 +26,8 @@ select json_array_elements(j_r) j
 select j -> 'элемент' ->> 'value' "wikidata",
        j -> 'элементLabel' ->> 'xml:lang' "язык",
        j -> 'элементLabel' ->> 'value' "название",
-       j -> 'типДорогиLabel' ->> 'value' "тип"
+       j -> 'типДорогиLabel' ->> 'value' "тип",
+       j -> '№' ->> 'value' "№"
 from element;
 
 refresh materialized view "Бирюлёвский дендропарк: WikiData"."Дорожно-тропиночная сеть";
