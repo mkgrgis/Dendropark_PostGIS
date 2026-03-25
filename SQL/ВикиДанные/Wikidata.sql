63610ed0f5cd026d683a695af48ae00b7469000a
Михаил <aksenov.mihail@bk.ru>
2026-03-25 07:35:28 +0300

    Расширение данных ДТС

--- SQL/ВикиДанные/Wikidata.sql
+++ SQL/ВикиДанные/Wikidata.sql
@@ -6,15 +6,17 @@ select
   (http_get(
     'https://query.wikidata.org/sparql?query=' || 
     urlencode(
-      'SELECT ?элемент ?элементLabel ?типДорогиLabel ?№
+      'SELECT ?картинка ?элемент ?элементLabel ?типДорогиLabel ?№ ?Категория
 WHERE {
   ?элемент wdt:P361 wd:Q4087179 ; p:P361 ?stn. # Бирюлёвский дендропарк     
-  OPTIONAL { ?stn pq:P1545 ?№.}                # порядковый номер его части (из паспорта ОКН)
-  ?элемент wdt:P31 ?типДороги .                # тип объекта
+  OPTIONAL { ?stn pq:P1545 ?№. }               # порядковый номер его части (из паспорта ОКН)
+  OPTIONAL { ?элемент wdt:P373 ?Категория. }   # категория ВикиСклада
+  ?элемент wdt:P31 ?типДороги.                 # тип объекта
   ?типДороги wdt:P279* ?родитель.
-  FILTER (?родитель IN (wd:Q34442, wd:Q5004679)) . # тип объекта восходит к дорогам или тропам
+  OPTIONAL { ?элемент wdt:P18 ?картинка. }
+  FILTER (?родитель IN (wd:Q34442, wd:Q5004679, wd:Q174782)) . # тип объекта восходит к дорогам, тропам или площадям
   SERVICE wikibase:label { bd:serviceParam wikibase:language "ru" . }
-}'
+} '
     ) ||
     '&format=json'
   )).content::json -> 'results' -> 'bindings' j_r
@@ -23,11 +25,15 @@ element as (
 select json_array_elements(j_r) j
   from wikidata_json
 )
-select j -> 'элемент' ->> 'value' "wikidata",
+select split_part(j -> 'элемент' ->> 'value', '/', -1) "Q",       
        j -> 'элементLabel' ->> 'xml:lang' "язык",
        j -> 'элементLabel' ->> 'value' "название",
        j -> 'типДорогиLabel' ->> 'value' "тип",
-       j -> '№' ->> 'value' "№"
-from element;
+       (j -> '№' ->> 'value')::semver "№",
+       j -> 'Категория' ->> 'value' "Категория",
+       j -> 'картинка' ->> 'value' "Изображение",
+       j -> 'элемент' ->> 'value' "wikidata"
+from element
+order by "№", "название";
 
 refresh materialized view "Бирюлёвский дендропарк: WikiData"."Дорожно-тропиночная сеть";
